1c15fe9a78c1a4a059628f8a9d7595048cd47af1
hayrunnisa25 <hayrunnisa.kahraman@picussecurity.com>
2025-08-05 09:36:27 +0300

    yeni değişiklik

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import { useEffect } from 'react';
-import { useState } from 'react';
+import {  useState } from 'react';
 
 const App = () => {
-  const [data, setData] = useState({});
+  const [data, setData] = useState({ results:[] });
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=3&offset=3")
-    .then( x => x , json()).then(x => setData(X))
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=3&offset=0")
+      .then((x) => x.json())
+      .then((x) => setData(x));
   }, []);
-  return <pre>{JSON.stringify(data,null,2)}
-  </pre>;
+
+  return (
+    <div style={{ display: "flex", flexDirection: "column"}}>
+      {data.results.map ((e) => (
+        <div
+         key={e.name}//
+          style={{
+           maxWidth: "min-content",
+           padding: "4px",
+           margin: "4px",
+           border: "1px solid black",
+          }}
+        >
+         <span>{e.name}</span>
+        </div>
+      )) }
+    </div>  
+  );
 };
 
 export default App;
