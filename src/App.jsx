fe665b638d8d5c825da7399b8deed00fae539937
hayrunnisa25 <hayrunnisa.kahraman@picussecurity.com>
2025-08-06 10:31:54 +0300

    yeni

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,19 @@
-import { useEffect } from 'react';
-import {  useState } from 'react';
+import {useEffect, useState } from 'react';
+import'./App.css';
 
-const PokemonCard = ({ e }) => { 
-  const [data, setData] = useState({});
-  useEffect(() => {
-    if(!e.name) return;
-   fetch("https://pokeapi.co/api/v2/pokemon/" + e.name )
-     .then((x) => x.json())
-     .then((x) => setData(x));
-  }, [e.name]);
+function App() {
+    const[pokemonList,setPokemonList] = useState([]);
+    const[loading,setLoading] =useState(true);
 
- return ( 
-  <div
-    style={{
-      Width: "800px",
-      padding: "12px",
-      margin: "12px",
-      border: "4px solid pink",
-     }}
-  >
-    <span>{e.name}</span>
-    {data.sprites?.front_default && (
-    <img src={data.sprites.front_default} />
-    )}
-    </div>
- );
-};
-
- const PAGE_SIZE=3
-const App = () => {
-  const [data, setData] = useState({});
-  const [page, setPage] = useState(0)
-  useEffect (() => {
-    fetch(
-      "https://pokeapi.co//api/v2/pokemon?limit=" + 
-       PAGE_SIZE + 
-       "&offset=" +
-       PAGE_SIZE * page,
-    )
-     .then((x) => x.json())
-     .then((x) => setData(x));
-  }, [page]);
-
-  return (
-    <div style={{ display: "flex", flexDirection: "column", backgroundColor: "grey", minHeight: "100vh",}}>
-      <h1 style={{ marginTop: "20px", color: "pink"}}>Pokemon Kataloğu</h1>
-      {data.results?.map((e) => (
-       <PokemonCard key={e.name} e={e} />
-      ))}
-     <div style={{ display: "flex", flexDirection: "row"}}>
-       <button onClick={() => setPage((ps) => ps - 1)}>prev</button>
-       <button onClick={() => setPage((ps) => ps + 1)}>next</button>
-      </div>
-    </div>
-  );
-};
-     
-export default App;
+    useEffect(() =>{
+    const fetchPokemons =async () =>{
+        try {
+            const res =await
+    fetch('htps://pokeapi.co/api/v2/pokwmon?limit=20');
+    const data = await res.json();
+    
+        }
+    }
+    }
+)
+}
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React from "react";
 
-function PokemonCard() {
-  return (
-    <div>
-      <h2>Merhaba Hayrünnisa!</h2>
-      <p>Nasılsın?</p>
-    </div>
+const PokemonCard = ({pokemon}) =>{
+ return ( 
+  <div className="card">
+  <img src={pokemon.sprites.front_default}
+  alt={pokemon.name}/>
+  <h2>{pokemon.name.charAt(0).toUpperCasel()+pokemon.name.slice(1)}</h2>
+  <p>#{pokemon.id}</p>
+  </div>
   );
-}
+};
 
-export default PokemonCard;
+export default PokemonCard
+
+
