f86fb5e62c48f8df170a6d19d600165fd89e9f02
hayrunnisa25 <hayrunnisa.kahraman@picussecurity.com>
2025-08-11 11:02:39 +0300

    flex düzeni sayfa sayısı ekleme

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import { useEffect, useState } from "react";
 export default function App() {
   const [data, setData] = useState([]);
   const [search, setSearch] = useState("");
+  const [page, setPage] = useState(1);
   const [offset, setOffset] = useState(0);
   const limit = 3;
 
  useEffect(() => {
-    fetch ('https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}')
+    fetch ('https://pokeapi.co/api/v2/pokemon?limit=' + limit + '&offset=' + offset)
       .then((r) => r.json()) 
       .then((d) =>
  Promise.all(d.results.map((p) => fetch(p.url).then((r) => r.json())))
@@ -27,10 +28,19 @@ export default function App() {
         value={search}
         onChange={(e) => setSearch(e.target.value)}
       />
-      <div style={{ display: "flex", justifyContent: "center", gap: "10px" }}>
+      <div style={{ display: "flex", justifyContent: "center", gap: "10px", marginTop: "40px" }}>
         {filtered.map((p) => (
-          <div key={p.id}>
-            <img src={p.sprites.front_default} alt={p.name} />
+         <div
+         key={p.id}
+         style={{
+         border: "1px solid black",
+         padding: "10px",
+         borderRadius: "8px",
+         textAlign: "center",
+         width: "120",
+         }}
+         >
+          <img src={p.sprites.front_default} alt={p.name} />
             <div>{p.name}</div>
             <div>#{p.id}</div>
           </div>
@@ -38,9 +48,11 @@ export default function App() {
       </div>
       <div>
         <button onClick={() => setOffset(prev => Math.max(prev - limit, 0))}>
-          Geri
+        Geri
         </button>
-        <button onClick={() => setOffset(prev => prev + limit)}>İleri</button>
+        <span>{page}</span>
+        <button onClick={() => setOffset(prev => prev + limit)}>İleri
+         </button>
       </div>
     </div>
   );
