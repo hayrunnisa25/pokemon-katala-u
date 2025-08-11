03661205a817730ed598c6a1c8cc4a9b5e1cb91b
hayrunnisa25 <hayrunnisa.kahraman@picussecurity.com>
2025-08-11 04:31:10 +0300

    yeni

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,47 @@
-import {useEffect, useState } from 'react';
-import'./App.css';
+import { useEffect, useState } from "react";
 
-function App() {
-    const[pokemonList,setPokemonList] = useState([]);
-    const[loading,setLoading] =useState(true);
+export default function App() {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+  const [offset, setOffset] = useState(0);
+  const limit = 3;
 
-    useEffect(() =>{
-    const fetchPokemons =async () =>{
-        try {
-            const res =await
-    fetch('htps://pokeapi.co/api/v2/pokwmon?limit=20');
-    const data = await res.json();
-    
-        }
-    }
-    }
-)
+ useEffect(() => {
+    fetch ('https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}')
+      .then((r) => r.json()) 
+      .then((d) =>
+ Promise.all(d.results.map((p) => fetch(p.url).then((r) => r.json())))
+      )
+      .then(setData);
+  }, [offset]);
+
+  const filtered = data.filter((p) =>
+    p.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Pokémon Kataloğu</h1>
+      <input
+        placeholder="Ara..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <div style={{ display: "flex", justifyContent: "center", gap: "10px" }}>
+        {filtered.map((p) => (
+          <div key={p.id}>
+            <img src={p.sprites.front_default} alt={p.name} />
+            <div>{p.name}</div>
+            <div>#{p.id}</div>
+          </div>
+        ))}
+      </div>
+      <div>
+        <button onClick={() => setOffset(prev => Math.max(prev - limit, 0))}>
+          Geri
+        </button>
+        <button onClick={() => setOffset(prev => prev + limit)}>İleri</button>
+      </div>
+    </div>
+  );
 }
